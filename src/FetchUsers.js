9c1815194813ff6f3a04043a3f70461bed76538a
Evelyn Oliveira <evelynolives@gmail.com>
2023-11-02 23:24:44 -0300

    update fetch component

--- src/FetchUsers.js
+++ src/FetchUsers.js
@@ -1,25 +1,54 @@
-import React, {useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { BASE_URL } from './constants/urls'
 
 const FetchUsers = () => {
-    const [user, setUsers] = useState([])
+    const [users, setUsers] = useState([])
 
     useEffect(() => {
-      const fetchUserData = async() => {
-        const resp = await fetch(BASE_URL)
-        const users = await resp.json()
-        setUsers(users)
-        console.log(users)
+        const fetchUserData = async () => {
+            const resp = await fetch(BASE_URL)
+            const users = await resp.json()
+            setUsers(users.results)
+            console.log(users.results)
+        }
 
-      }
-      fetchUserData()
-      })
-    
-      
+        fetchUserData()
+    }, [])
 
-  return (
-    <div>FETCH USERS</div>
-  )
+
+
+    return (
+        <div>
+            <h1>Users</h1>
+            <section>
+                {users.map((user) => {
+                    
+                    const { gender,
+                        name: {
+                            title,
+                            first,
+                            last 
+                        },
+                        dob: {
+                            age
+                        },
+                        registered: {
+                            date
+                        },
+                    } = user
+                    return (
+                        <div key={user.login.uuid}>
+                            <h3>{user.login.uuid} {title}. {first} {last} {gender} {age} {date}
+                            </h3>
+                        </div>
+                    )
+                }
+                )}
+
+
+            </section>
+        </div>
+    )
 }
 
 export default FetchUsers;
