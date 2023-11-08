e175c486199417ad825ca011d1e44a013aefd0ab
Evelyn Oliveira <evelynolives@gmail.com>
2023-11-08 14:37:56 -0300

    update user table

---
+++ src/Components/UserTable/UserTable.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+import './UserTable.css';
+
+const UserTable = () => {
+ const [users, setUsers] = useState([]);
+
+ useEffect(() => {
+    fetchUsers();
+ }, []);
+
+ const fetchUsers = async () => {
+    try {
+      const response = await fetch('https://randomuser.me/api/?results=10');
+      const data = await response.json();
+      setUsers(data.results);
+    } catch (error) {
+      console.error('Error fetching users:', error);
+    }
+ };
+
+ return (
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Title</th>
+          <th>Date</th>
+          <th>Age</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {users.map((user) => (
+          <tr key={user.login.uuid}>
+            <td>{user.login.uuid}</td>
+            <td>{user.name.first}</td>
+            <td>{user.name.last}</td>
+            <td>{user.name.title}</td>
+            <td>{user.dob.date}</td>
+            <td>{user.dob.age}</td>
+            <td>
+              <button>View profile</button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+ );
+};
+
+export default UserTable;
