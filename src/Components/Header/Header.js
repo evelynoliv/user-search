e581c33641c3c62f494e4a42737e3038a3da65fc
Evelyn Oliveira <evelynolives@gmail.com>
2023-11-13 17:43:17 -0300

    add search bar

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+    return (
+        <div>
+            <form class="search-bar">
+                <input type="text" placeholder="Search user..."/>
+                    
+            </form>
+        </div>
+    )
+}
+
+export default Header
